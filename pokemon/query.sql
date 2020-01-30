3944ea51886a1af98bc6b4da8374285c0165eebc
Emma Pang <emma.kpang@gmail.com>
2020-01-29 20:50:26 -0500

    added project report

--- pokemon/query.sql
+++ pokemon/query.sql
@@ -4,6 +4,13 @@ SELECT * FROM firstgen;
 SELECT * FROM secondgen;
 
 -- Query some specific information for fun!
+-- find only grass pokemon from the firstgen table
 SELECT *
 FROM firstgen
 WHERE type = 'grass';
+
+-- find secondgen pokemon with a height over 10 m and weight over 40 kg
+SELECT *
+FROM secondgen
+WHERE height_m >= 10
+and weight_kg >= 40;
