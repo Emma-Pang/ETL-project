e4fe62c181b17c8c04157806980c4eb1f61403a5
Emma Pang <emma.kpang@gmail.com>
2020-01-29 19:56:14 -0500

    connected to sql database and loaded dataframes into database

---
+++ pokemon/schema.sql
@@ -0,0 +1,17 @@
+-- Create Two Tables
+CREATE TABLE firstgen (
+ 	number INT PRIMARY KEY,
+ 	name TEXT,
+ 	type TEXT,
+	height_m INT,
+	weight_kg INT
+	
+);
+
+CREATE TABLE secondgen (
+ 	number INT PRIMARY KEY,
+ 	name TEXT,
+ 	type TEXT,
+	height_m INT,
+	weight_kg INT
+);
